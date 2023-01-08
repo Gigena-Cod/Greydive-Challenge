e261f4108000ee447b901dd6083a5a3caa4e64a5
Gigena-Cod <nahuel.gigena@hotmail.com>
2023-01-08 19:12:30 -0300

    More changes and features

--- src/components/CardResponse.component.tsx
+++ src/components/CardResponse.component.tsx
@@ -5,18 +5,18 @@ const CardResponseComponent = ({ full_name, email, birth_date, country_of_origin
         <div className="transition-all duration-300 select-none ease-in-out bg-[#EAFF6A]  hover:shadow p-4 rounded-lg card">
 
             <div className="flex gap-x-2 text-[#1F2023] name__full">
-                <span className="font-bold text">
+                <span className="min-w-[9rem] font-bold text">
                     Nombre completo:
                 </span>
-                <span className="value">
+                <span className="truncate value">
                     {full_name}
                 </span>
             </div>
             <div className="flex gap-x-2 text-[#1F2023] email">
-                <span className="font-bold  text">
+                <span className="min-w-[3rem] font-bold  text">
                     Email:
                 </span>
-                <span className="value">
+                <span className="truncate value">
                     {email}
                 </span>
             </div>
