727a498810caf3fb2a03a55a3d0b1f3045385ba2
Gigena-Cod <nahuel.gigena@hotmail.com>
2023-01-08 18:59:47 -0300

    More changes and features

--- src/pages/Home.view.tsx
+++ src/pages/Home.view.tsx
@@ -1,13 +1,13 @@
 import FormComponent from "../components/Form.component"
 
 const HomeView = () => {
-    return (
-        <div className="background p-8 h-screen home__view">
-        <div className="home__container">
-          <FormComponent/>
-        </div>
+  return (
+    <div className="background p-8 h-screen home__view">
+      <div className="mt-12 home__container">
+        <FormComponent />
       </div>
-    )
+    </div>
+  )
 }
 
 export default HomeView
